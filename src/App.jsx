157ec8208bbbdb3a637aa2151b7fc6f37238b03a
Jordi Turull <jordi.frontend@gmail.com>
2022-12-25 23:49:17 +0100

    feat: first weather test

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?lat=38.027106&lon=-1.139693&appid=4a30997d288e2ad2ace0882245357ff1&units=metric&lang=es"
+    )
+      .then((response) => response.json())
+      .then((data) => setWeatherData(data));
+  }, []);
+
+  const weatherElement = weatherData && weatherData.list[0];
+
+  // show the date in a human readable format
+  const date = weatherElement && new Date(weatherElement.dt * 1000);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="App-header">
+        {weatherData && (
+          <>
+            <div className="title">
+              <img
+                src={`http://openweathermap.org/img/w/${weatherElement.weather[0].icon}.png`}
+                alt={weatherElement.weather[0].description}
+              />
+              {weatherData.city.name}
+            </div>
+            <p>
+              {parseInt(weatherElement.main.temp, 10)} °C /{" "}
+              {weatherElement.main.humidity} %
+            </p>
+            <p>{date.toLocaleString()}</p>
+          </>
+        )}
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
